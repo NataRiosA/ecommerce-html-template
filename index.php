dc8c3a794bf6297f60820688b0f4ec423e2b10ff
natalia <nataliarios325@gmail.com>
2021-05-19 21:13:08 -0500

    second commit

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php include("login.html"); ?>
+<?php include("cart.html"); ?>
+<?php include("checkout.html"); ?>
+<?php include("contact.html"); ?>
+<?php include("index.html"); ?>
+<?php include("login.html"); ?>
+<?php include("my-account.html"); ?>
+<?php include("product.html"); ?>
+<?php include("product-list.html"); ?>
+<?php include("wishlist.html"); ?>
